eebb13328b3a6bf5c8ccac3c6ef42c362cfb8285
Shivani Jetty <shivanijetty@gmail.com>
2022-10-03 16:15:05 -0400

    commit tree b368d39adbfe0b6c4f26ce2b58c5c612e6682c32

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Code your solutions in this file
+for (let age = 30; age < 40; age++) {
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+}
+
+
+
+function wrapGifts(arrayOfGifts) {
+  for (let i = 0; i < arrayOfGifts.length; i++) {
+    console.log(`Wrapped ${arrayOfGifts[i]} and added a bow!`);
+    debugger;
+  }
+
+}
+// const myGifts = ["teddy bear", "drone", "doll"];
+// wrapGifts(myGifts);
+wrapGifts(["PS4", "ball", "cards"])
+
+
+
+
+
+function writeCards(arrayOfNames, event) {
+    let thankYous = []
+    for (let i = 0; i < arrayOfNames.length; i++) {
+        thankYous.push(`Thank you, ${arrayOfNames[i]}, for the wonderful ${event} gift!`)
+    }
+    return thankYous
+}
+
+function countDown(num) {
+        while (num >= 0) {
+        console.log(num--);
+    }
+}
